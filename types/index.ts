514eec8d8110cf11c75b4c212fbe50e9863c8186
3kingwangxin <wangxinnlg@126.com>
2023-12-22 14:01:53 +0800

    carcard

--- types/index.ts
+++ types/index.ts
@@ -9,3 +9,18 @@ export interface SearchManufacturerProps {
   manufacturer: string
   setManufacturer: (manufacturer: string) => void
 }
+export interface CarProps{
+  city_mpg: number
+  class: string
+  combination_mpg: number
+  cylinders: number
+  displacement: number
+  drive: string
+  fuel_type: string
+  id: number
+  highway_mpg: number
+  make: string
+  model: string
+  transmission: string
+  year: number
+}
